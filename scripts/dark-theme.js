407ddd6911b09eec74b29bde5817f184b4ff5712
Pablo Barbosa Silva <126425360+PabloPBS@users.noreply.github.com>
2023-06-22 14:52:55 -0300

    trocado a cor do background verde para amarelo

--- scripts/dark-theme.js
+++ scripts/dark-theme.js
@@ -17,8 +17,8 @@ theme.addEventListener('click', function() {
         logo.src = "images/logo/logo-white.png"
 
         //Mundado o background da sessão projetos e tecnologias
-        sessaoTecnologias.style.backgroundImage = "url(images/background-svg/tecnologias-dark-bg.svg)"
-        sessaoProjetos.style.backgroundImage = "url(images/background-svg/projetos-dark.svg)"
+        sessaoTecnologias.style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%234a402f' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%236e5a38' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%23917540' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%23b58f49' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%23D9AA52' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E\")"
+        sessaoProjetos.style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%23b58f49' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%23917540' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%236e5a38' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%234a402f' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%23262526' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E\")"
     } else {
         //Mudando o ícone para a lua
         theme.classList.replace("fa-sun", "fa-moon")
@@ -32,7 +32,7 @@ theme.addEventListener('click', function() {
         logo.src = "images/logo/logo.png"
 
         //Mudando o background da sessão projeto e tecnologias
-        sessaoTecnologias.style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%23d6dbd0' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%23bbc7b6' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%239fb49d' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%2384a083' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%23688C6A' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E\")"
-        sessaoProjetos.style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%2384a083' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%239fb49d' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%23bbc7b6' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%23d6dbd0' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%23F2EFE9' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E\")"
+        sessaoTecnologias.style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%23ede1cb' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%23e8d3ad' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%23e3c68e' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%23deb870' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%23D9AA52' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E\")"
+        sessaoProjetos.style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%23deb870' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%23e3c68e' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%23e8d3ad' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%23ede1cb' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%23F2EFE9' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E\")"
     }
 })
